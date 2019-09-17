07b7306b9c049f112b266b2264af06e288f86dd8
zongxili <zongxiliwork@gmail.com>
2019-09-17 14:09:44 -0700

    q5 added

--- 4_queries/q1.sql
+++ 4_queries/q1.sql
@@ -1,6 +1,5 @@
 SELECT count(assistance_requests.*) as total_assistances, teachers.name
 FROM assistance_requests
--- need to check how come we need a JOIN here
 JOIN teachers ON teachers.id = teacher_id
 WHERE name = 'Waylon Boehm'
 GROUP BY teachers.name;
--- 4_queries/q2.sql
+++ 4_queries/q2.sql
@@ -1,10 +1,10 @@
-SELECT count(*) AS total_assistances, students.name
-FROM assistance_requests
-JOIN students on students.id = assistance_requests.student_id
-WHERE students.name = 'Elliot Dickinson'
-GROUP BY students.name;
+-- Here is my own answer, also works
+-- SELECT count(*) AS total_assistances, students.name
+-- FROM assistance_requests
+-- JOIN students on students.id = assistance_requests.student_id
+-- WHERE students.name = 'Elliot Dickinson'
+-- GROUP BY students.name;
 
--- need to check how come we need do COUNT(assistance_requests.*)
 SELECT count(assistance_requests.*) as total_assistances, students.name
 FROM assistance_requests
 JOIN students ON students.id = student_id
