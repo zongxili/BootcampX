04119f4663936307338728943464dcb556b632f3
zongxili <zongxiliwork@gmail.com>
2019-09-16 19:22:57 -0700

    Back to normal

--- 3_queries_group-by/q6.sql
+++ 3_queries_group-by/q6.sql
@@ -10,4 +10,4 @@ JOIN assignments ON assignments.id = assignment_id
 WHERE end_date IS NULL
 GROUP BY student
 HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
-ORDER BY average_assignment_duration;asdf
+ORDER BY average_assignment_duration;
